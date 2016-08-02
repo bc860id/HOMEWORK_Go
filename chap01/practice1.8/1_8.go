4a878b23dbc9466d838a4f803f3bca12abfacd19
bc860id <ikuhisa_okamoto@technologies.ricoh.co.jp>
2016-08-02 16:08:52 +0900

    課題1.8の解答。

---
+++ chap01/practice1.8/1_8.go
@@ -0,0 +1,38 @@
+
+// // 第1章 練習問題1.8
+package main
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var prefix = []string{ "http://", "https://" }
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if (strings.HasPrefix(url, prefix[0]) == false) &&
+		   (strings.HasPrefix(url, prefix[1]) == false) {
+				url = prefix[0] + url
+				fmt.Fprintf(os.Stderr, "\n\x1b[41;33mappend prefix\x1b[0m: %v\n", url)
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+	}
+}
+
+
---
+++ chap01/practice1.8/test_1_8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# proxy越えはパスワードをプレーンで書いておいたり入力したりする無防備さに耐えれずやらない.
+#./1_8 www.ricoh.co.jp http://blogs.rintra.net/event-calendar/ http://www.ricoh.co.jp blogs.rintra.net/event-calendar/
+./1_8 www.ricoh.co.jp http://blogs.rintra.net/event-calendar/ http://www.ricoh.co.jp blogs.rintra.net/event-calendar/	\
+2> err.txt
+
