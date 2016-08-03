b6daace9c7ef23d5c560f2f73fce18a4d3aba6f3
bc860id <ikuhisa_okamoto@technologies.ricoh.co.jp>
2016-08-03 15:45:10 +0900

    課題1.11の回答。

---
+++ chap01/practice_01.11/1_11.go
@@ -0,0 +1,47 @@
+
+// base program written on P19 gopl.io/ch1/fetchall
+package main
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Printf("TOTAL URL:%d\n", (len(os.Args) - 1))
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	i := 1
+	for range os.Args[1:] {
+		fmt.Printf("%06d ", i)
+		fmt.Println(<-ch)
+		i++
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
+
+
