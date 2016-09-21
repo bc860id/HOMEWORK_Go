a03441e654aa732f6de21eb4108159178bbd35c1
miq <qstream1008io@gmail.com>
2016-09-22 00:17:14 +0900

    課題4.1の回答。

--- chap04/practice_04.01/4_1.go
+++ chap04/practice_04.01/4_1.go
@@ -11,21 +11,25 @@ func main() {
 	var bit_num int
 
 	//fmt.Println(len(os.Args))
-	if ( len(os.Args) < 2 ) {
+	if ( len(os.Args) < 3 ) {
 		return
 	}
 
-	c := sha256.Sum256([]byte(os.Args[1]))
-	//c := [...]byte{0: 0xFF, 31: 0x0E}
+	c1 := sha256.Sum256([]byte(os.Args[1]))
+	c2 := sha256.Sum256([]byte(os.Args[2]))
+	/*
+	c1 := [...]byte{0: 0xFF, 31: 0x0E}
+	c2 := [...]byte{0: 0xFF, 31: 0x0C}
+	*/
 
-	for i := 0; i < len(c); i++ {
-		b := c[i]
+	for i := 0; i < len(c1); i++ {
+		b := (c1[i] ^ c2[i])
 		for ; b > 0; bit_num++ {
 			b = (b & (b - 1))
 		}
 	}
 
-	fmt.Printf("total bits:%d\n", bit_num)
+	fmt.Printf("total diff bits:%d\n", bit_num)
 }
 
 
--- chap04/practice_04.01/test_4_1.sh
+++ chap04/practice_04.01/test_4_1.sh
@@ -7,9 +7,10 @@ FILE_NAME=4_1
 go build ${FILE_NAME}.go
 
 # execute
-./${FILE_NAME} X
-./${FILE_NAME} x
-./${FILE_NAME} Ab12CxYz
+./${FILE_NAME} X x
+./${FILE_NAME} x x
+./${FILE_NAME} Ab12CxYz ab
+./${FILE_NAME} Ab12CxYz Ab12CxYz
 
 #----- [EOF] -----#
 
