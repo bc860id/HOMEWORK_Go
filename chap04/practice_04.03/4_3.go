1f69d4739d8982358b761a7da535f3d105b6ea88
miq <qstream1008io@gmail.com>
2016-09-22 14:27:27 +0900

    課題4.3の回答。

---
+++ chap04/practice_04.03/4_3.go
@@ -0,0 +1,20 @@
+
+// 第4章 練習問題4.2
+package main
+import (
+	"fmt"
+)
+
+var a = [...]int{0, 1, 2, 3, 4, 5}
+
+func main() {
+	reverse(&a)
+	fmt.Println(a)
+}
+
+func reverse(s *[len(a)]int) {
+	for i, j := 0, (len(s) - 1); i < j; i, j = (i + 1), (j - 1) {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
